2035ac5b052fca2c07ceb29fcecd6fdd96f5f6bf
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2022-05-25 23:20:52 +0530

    Electron application: Button controls

--- application/render/script/index.js
+++ application/render/script/index.js
@@ -1,6 +1,21 @@
-textFieldIP = document.getElementById("ip-address");
-connectButton = document.getElementById("connect");
+// DOM linking
+const textFieldIP = document.getElementById("ip-address");
+const connectButton = document.getElementById("connect");
 
+const upButton = document.getElementById("up");
+const downButton = document.getElementById("down");
+
+const forwardButton = document.getElementById("forward");
+const backwardButton = document.getElementById("backward");
+const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
+
+const wingLightsButton = document.getElementById("wing-lights");
+const tailLightButton = document.getElementById("tail-light");
+
+let isDeviceConnected = false;
+
+// Validate IP address entered by the user
 function validateIPAddress(IP) {
     const PatternIP = /^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
 
@@ -18,11 +33,40 @@ connectButton.addEventListener("click", () => {
     };
 
     fetch(`http://${textFieldIP.value}/`)
-        .then(response => response.json())
-        .then(data => console.log(data))
+        .then(() => {
+            isDeviceConnected = true;
+        })
         .catch(error => {
             alert("Unable to connect to the device!")
         });
 })
 
 
+// Movement related controls
+const control = [upButton, downButton, forwardButton, backwardButton, leftButton, rightButton];
+const controlGETPath = ['up', 'down', 'forward', 'backward', 'left', 'right'];
+
+for (let i = 0; i < control.length; i++) {
+    control[i].addEventListener('click', () => {
+        console.log(controlGETPath[i]);
+
+        if (isDeviceConnected)
+            fetch(`http://${textFieldIP.value}/${controlGETPath[i]}`);
+    })
+}
+
+// LED light controls
+wingLightsButton.addEventListener("click", () => {
+    console.log("LED lights: Wing");
+
+    if (isDeviceConnected)
+        fetch(`http://${textFieldIP.value}/wing-lights`);
+})
+
+
+tailLightButton.addEventListener("click", () => {
+    console.log("LED lights: Tail");
+
+    if (isDeviceConnected)
+        fetch(`http://${textFieldIP.value}/tail-lights`);
+})
