1804bb0ae518eca1fa5b274a47c99f35570e8b72
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2022-05-25 23:28:12 +0530

    Electron application: Message modal

--- application/render/script/index.js
+++ application/render/script/index.js
@@ -28,7 +28,7 @@ function validateIPAddress(IP) {
 // Establish wireless connection over WiFi
 connectButton.addEventListener("click", () => {
     if (!validateIPAddress(textFieldIP.value)) {
-        alert("Invalid IP address");
+        displayMessageViaModal("Invalid IP address!", "Please check the entered IP address.");
         return;
     };
 
@@ -37,7 +37,7 @@ connectButton.addEventListener("click", () => {
             isDeviceConnected = true;
         })
         .catch(error => {
-            alert("Unable to connect to the device!")
+            displayMessageViaModal("Device failed to connect!", "Please check if the device is powered on and try again.");
         });
 })
 
@@ -70,3 +70,18 @@ tailLightButton.addEventListener("click", () => {
     if (isDeviceConnected)
         fetch(`http://${textFieldIP.value}/tail-lights`);
 })
+
+
+// Generic modal to elegantly display message
+
+// "Low battery voltage!"
+// "Please land the bicopter as soon as possible."
+
+// "Low signal strenght!"
+// "Turn the bicopter around to avoid loosing connection."
+
+function displayMessageViaModal(messageTitle, messageContent) {
+    document.getElementsByClassName('modal-title')[0].innerText = messageTitle;
+    document.getElementsByClassName('modal-body')[0].innerText = messageContent;
+    document.getElementById('show-modal').click();
+}
